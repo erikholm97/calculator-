fea5b7a360b220e8f01ba4bd88aad2eaa4207942
Erik <erik.holm@fbinhouse.se>
2019-10-07 14:25:10 +0200

    Fixed functions.

--- miniraknare.js
+++ miniraknare.js
@@ -1,7 +1,35 @@
        const calculator = document.querySelector('.calculator')
        const keys = calculator.querySelector('.calculator__keys')
        const display = document.querySelector('.calculator__display')
-      
+       let operator = null;
+
+
+       const calculate = (n1, n2) => {
+       let result = ''
+        console.log(n1)
+        console.log(n2)
+        console.log(operator)
+        if (operator === 'add') {
+          result = n1 + n2
+        } else if (operator === 'subtract') {
+          result = n1 - n2
+        } else if (operator === 'multiply') {
+          result = n1 * n2
+        } else if (operator === 'divide') {
+          result = n1 / n2
+        }
+        
+        return result
+      }
+      const checkDecimal = (decimalNumber) =>{
+        
+        if(decimalNumber.includes("..")){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }
        
     keys.addEventListener('click', e => {
        if (e.target.matches('button')) {
@@ -9,11 +37,12 @@
         const action = key.dataset.action // Om tangenttrycket inte har någon "data-action" så måste det vara ett nummer. 
         const keyContent = key.textContent // för att spara vilket knapptryck som gjorts
         const displayedNum = display.textContent // Sparar vilket nummer som finns på displayen. 
-        const previousKeyType = calculator.dataset.previousKeyType
+        
+       
         
     
         if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+            if (displayedNum === '0') {
                 display.textContent = keyContent;
                
             } 
@@ -21,36 +50,37 @@
             else {
                 display.textContent = displayedNum + keyContent
             }
-        // console.log('number key!')
-        if ( 
-        
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-            ){
-              /*  
-              Array.from(key.parentNode.children)
-                .forEach(k => k.classList.remove('is-depressed'))
-                // Add custom attribute
-                calculator.dataset.previousKeyType = 'operator'
-              */  
-            
-             
-            }
-        
         }
         if (action === 'decimal') { // Om action är decimal. 
-            display.textContent = displayedNum + '.' // Nummret som visas får ett decimal tecken. 
+            display.textContent = displayedNum + '.' // Nummret som visas får ett decimal tecken.
+            bool = checkDecimal(display.textContent)
+            console.log(bool)
         }
         if(action === 'clear'){
             console.log("pressed clear");
             display.textContent = 0
         }
         
-        if (action === 'decimal') {
-         display.textContent = displayedNum + '.'
+        if (action === 'add') {
+          display.textContent = displayedNum + '+'
+          operator = 'add';
+
         }
+        if(action === 'subtract'){
+          display.textContent = displayedNum + '-'
+          operator = 'subtract';
+        }
+        if(action === 'multiply'){
+          display.textContent = displayedNum + '*'
+          operator = 'multiply';
+          
+        }
+        if(action === 'divide'){
+          display.textContent = displayedNum + '/'
+          operator = 'divide';
+          
+        }
+
 
         if (action === 'clear') {
         console.log('clear key!')
@@ -58,32 +88,14 @@
 
         if (action === 'calculate') {
         console.log('equal key!')
-            
-         const firstValue = calculator.dataset.firstValue
-         console.log(firstValue)
-         const operator = calculator.dataset.operator
-         const secondValue = displayedNum
-         console.log(secondValue)
-                
-            display.textContent = calculate(firstValue, operator, secondValue);
-
-
+        const values = displayedNum.match(/[+-]?\d+(\.\d+)?/g).map(n => parseFloat(n));
+        console.log(values)
+        display.textContent = calculate(values[0], values[1]);
+        console.log(display.textContent)
+        
+      
         }
-        const calculate = (n1, operator, n2) => {
-            let result = ''
-            
-            if (operator === 'add') {
-              result = n1 + n2
-            } else if (operator === 'subtract') {
-              result = n1 - n2
-            } else if (operator === 'multiply') {
-              result = n1 * n2
-            } else if (operator === 'divide') {
-              result = n1 / n2
-            }
-            
-            return result
-          }
+        
     
     }
     
